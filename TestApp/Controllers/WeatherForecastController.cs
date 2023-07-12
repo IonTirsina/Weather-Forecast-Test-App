77e7da180dafd999271dae609dbbc0e8b19cd17f
itirsina98 <itirsina98@gmail.com>
2023-07-12 20:58:29 +0300

    Added a new endpoint for getting weather forecase for a time range

--- TestApp/Controllers/WeatherForecastController.cs
+++ TestApp/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using TestApp.Enums;
 using TestApp.Services;
 
@@ -28,7 +29,7 @@ namespace TestApp.Controllers
         {
             var periodInDays = (int)period;
 
-            var weatherForecast = _weatherForecastService.get(periodInDays);
+            var weatherForecast = _weatherForecastService.getForDays(periodInDays);
 
             return Ok(weatherForecast);
         }
@@ -36,9 +37,29 @@ namespace TestApp.Controllers
         [HttpGet("{days:int}")]
         public ActionResult<IEnumerable<WeatherForecast>> GetForDays(int days)
         {
-            var weatherForecast = _weatherForecastService.get(days);
+            try
+            {
+                var weatherForecast = _weatherForecastService.getForDays(days);
 
-            return Ok(weatherForecast);
+                return Ok(weatherForecast);
+            } catch(ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<WeatherForecast>> GetForDateRange([FromQuery][Required] DateTime fromDate, [FromQuery][Required] DateTime toDate)
+        {
+            try
+            {
+                var weatherForecast = _weatherForecastService.getForDateRange(fromDate, toDate);
+
+                return Ok(weatherForecast);
+            } catch(ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- TestApp/Services/WeatherForecastService.cs
+++ TestApp/Services/WeatherForecastService.cs
@@ -2,11 +2,33 @@
 {
     public class WeatherForecastService
     {
-        public IEnumerable<WeatherForecast> get(int days)
+        public IEnumerable<WeatherForecast> getForDays(int days)
         {
+            if (days <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Number of days should be greater than 0");
+            }
+
             return Enumerable.Range(1, days).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Controllers.WeatherForecastController._summaries[Random.Shared.Next(Controllers.WeatherForecastController._summaries.Length)]
+            }).ToArray();
+        }
+
+        public IEnumerable<WeatherForecast> getForDateRange(DateTime fromDate, DateTime toDate)
+        {
+            if (fromDate >= toDate)
+            {
+                throw new ArgumentException("Dates are in wrong consecutiveness");
+            }
+
+            var differenceInDays = toDate.Subtract(fromDate).Days;
+            
+            return Enumerable.Range(1, differenceInDays).Select(index => new WeatherForecast
+            {
+                Date = fromDate.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Controllers.WeatherForecastController._summaries[Random.Shared.Next(Controllers.WeatherForecastController._summaries.Length)]
             }).ToArray();
